b7540e0ec39dd92d57c7a49bb2c829ef4ceeb934
Shaban Buja <94648882+banibuja@users.noreply.github.com>
2024-03-23 02:49:23 +0100

    mm

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {Page1} from './Page1';
+import { LoginForm as Login } from './LoginForm';
+import {Dashboard}  from './Dashboard';
+// import '@fortawesome/fontawesome-free/css/all.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <BrowserRouter>
+      <Routes>
+        
+        <Route path="/" element={<Page1 />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
